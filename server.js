601b29df11a31d6bb7c32d071d3075ac07fedf14
Serguei <sbalanovich@college.harvard.edu>
2014-11-30 17:02:58 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var express = require('express')
+svar express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var file = new(static.Server)();
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  file.serve(req, res);
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
