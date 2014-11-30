8190c137756b289aae3e32335202154bb1938a94
Serguei <sbalanovich@college.harvard.edu>
2014-11-30 17:57:19 -0500

    made work with express

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+var express = require('express')
 var static = require('node-static');
 var http = require('http');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
 var file = new(static.Server)();
-var app = http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(5000);
+app.get('/', function(request, response) {
+  file.serve(request, response);
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+
+
